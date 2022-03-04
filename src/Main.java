76cb7df8ac4f8833e28b48b7533a44f94367eb7d
Júlia <juhbotelho.contato@gmail.com>
2022-03-04 14:39:39 -0300

    Criando classe abstrata Conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -14,6 +15,6 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
-    }
 
+    }
 }
